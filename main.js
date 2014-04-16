244c4cfed9cab7a9a516f69dc4a2b5aef8d4b6e1
Nosthertus <nosthertus17@hotmail.com>
2014-04-16 17:14:01 -0430

    Html dynamic data.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ express version.
 //Load modules.
 var express = require('express'),
 	socket = require('socket.io'),
+	swig = require('swig'),
 	fs = require('fs');
 
 //Load config.
@@ -19,19 +20,42 @@ console.log('Configuration loaded.');
 //Initiate express module in app.
 var app = express();
 
+// app.get('/', function(request, response)
+// {
+// 	fs.readFile('./views/index.html', function(error, data)
+// 	{
+// 		if(error)
+// 		{
+// 			response.send('View cannot be rendered.');
+// 		}
+
+// 		response.type('html');
+// 		response.send(data);
+// 	});
+// });
+
+var test = "Hello";
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// app.set('view cache', false);
+swig.setDefaults(
+{
+	cache: false
+});
+
 app.get('/', function(request, response)
 {
-	fs.readFile('./views/index.html', function(error, data)
+	response.render('index', 
 	{
-		if(error)
-		{
-			response.send('View cannot be rendered.');
-		}
-
-		response.type('html');
-		response.send(data);
+		//Var to be named in the render : value;
+		'test': test
 	});
 });
+
 
 //Run the app.
 app.listen(port);
